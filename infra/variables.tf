cc3ec1c40532829ee15d91f46b8ecf9e65047f98
Lucas Ferraz <lucasferrazsilva@hotmail.com>
2025-05-12 20:34:39 -0300

    ajusta arquivos

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,14 +1,4 @@
-variable "aws_region" {
-  description = "Região AWS onde os recursos serão criados"
-  type        = string
-}
-
 variable "bucket_name" {
   description = "Nome do bucket S3"
-  type        = string
-}
-
-variable "environment" {
-  description = "Ambiente (dev/prod)"
   type        = string
 }
